d329ba2103bb78fdf1aa01247b1a0e3352b2dd66
john <john@entelo.com>
2016-07-03 18:16:30 -0700

    localized user edit form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
+  http_basic_authenticate_with name: "hi", password: "comeonin"
   
   def index
     @user_count = User.count
+  end
+  
+  def about
   end
   
 end
