a656507ce9b0dbcc45a463f4652d868f1385a2af
john <john@entelo.com>
2016-07-13 01:38:07 -0700

    Flat UI, simpler homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   http_basic_authenticate_with name: "hi", password: "comeonin"
   
+  skip_before_filter :go_complete_profile, :only => [:instructions]
+  
   def index
   end
   
