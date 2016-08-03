a23af863c1e3a78a059d96f021bf77114ad33dcd
john <john@entelo.com>
2016-08-03 01:36:19 -0700

    fixed specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   
-  it { should validate_presence_of :uid }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
   
 end
