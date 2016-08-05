68e2cba424feaeeb3443c5bb87b168bafda91b60
Nick Messick <nmessick@gmail.com>
2016-08-04 20:42:35 -0700

    Trying out skipping CSRF check for Users::RegistrationsController#create

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
+skip_before_filter :verify_authenticity_token, :only => :create
+
 
   # GET /resource/sign_up
   def new
