a243a6953da4195cc7a9ea95322a8b4bb1f9682f
ghuff <ghuff@teslamotors.com>
2016-08-07 05:20:01 -0700

    Relax phone number only unique index to allow same phone user with different roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805212014) do
+ActiveRecord::Schema.define(version: 20160807114729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,7 +142,7 @@ ActiveRecord::Schema.define(version: 20160805212014) do
     t.integer  "language",                                            default: 0,     null: false
     t.boolean  "available",                                           default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["phone_number_normalized"], name: "index_users_on_phone_number_normalized", unique: true, using: :btree
+    t.index ["phone_number_normalized", "name"], name: "index_users_on_phone_number_normalized_and_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
