df9c1b7255e81b5143baf1cb9d4310312934163b
Nick Messick <nmessick@gmail.com>
2016-08-13 23:45:56 -0700

    update User's roles in a concern, so update method can be shared

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::AdminApplicationController
   include UserParams
+  include UserRoles
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
@@ -26,21 +27,18 @@ class Admin::UsersController < Admin::AdminApplicationController
 
   # # PATCH/PUT /users/1
   # # PATCH/PUT /users/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @user.update(user_params)
-  #
-  #       format.html do
-  #         redirect_to admin_users_path, notice: 'User was successfully updated.', locale: I18n.locale.to_s
-  #       end
-  #       format.json { render :show, status: :ok, location: @user }
-  #     else
-  #       @type = session['user_type']
-  #
-  #       format.html { render :edit }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def update
+    respond_to do |format|
+      if update_user_roles(params) && @user.update(user_params)
+        format.html do
+          redirect_to admin_users_path, notice: 'User was successfully updated.', locale: I18n.locale.to_s
+        end
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 end
