a0786959681cdf9b19bb171c6a0407a30a9bed47
Gerald <gerald.huff@gmail.com>
2016-08-29 22:30:16 -0700

    Filter ride markers on the map to match the table. BONUS: bounce map marker when you mouse over the car icon in the table

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -5,16 +5,19 @@ function getStyleRules(prefix, names) {
   var result = {};
   for (var i = document.styleSheets.length; --i >= 0; ) {
     var ix, sheet = document.styleSheets[i];
-    if (sheet.cssRules !== null) {
-      for (ix = sheet.cssRules.length; --ix >= 0;) {
-        var selector = sheet.cssRules[ix].selectorText;
-        if (selector !== undefined && selector.startsWith(prefix)) {
-          var name = selector.replace(prefix, '');
-          if (names.indexOf(name) > -1)
-            result[name] = sheet.cssRules[ix].style;
+    try {
+      if (sheet.cssRules !== null) {
+        for (ix = sheet.cssRules.length; --ix >= 0;) {
+          var selector = sheet.cssRules[ix].selectorText;
+          if (selector !== undefined && selector.startsWith(prefix)) {
+            var name = selector.replace(prefix, '');
+            if (names.indexOf(name) > -1)
+              result[name] = sheet.cssRules[ix].style;
+          }
         }
       }
     }
+    catch(e) {} // stylesheets from other domains sometimes can't be examined, skip
   }
   return result;
 }
