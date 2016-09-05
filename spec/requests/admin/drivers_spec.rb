6421614df4c296892a694905f25b3f1f5b3bb85a
john <john@entelo.com>
2016-09-04 23:35:18 -0700

    fixed specs, added a few

---
+++ spec/requests/admin/drivers_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Drivers", type: :request do
+  describe "GET /admin/drivers" do
+
+    it "redirects if you're not logged in" do
+      get admin_drivers_path
+      expect(response).to have_http_status(302)
+    end
+
+    it "redirects if you're logged in as a non-admin" do
+      user = create(:user)
+      sign_in user
+
+      get admin_drivers_path
+      expect(response).to have_http_status(302)
+    end
+
+    it "succeeds if you're logged in as an admin" do
+      user = create(:admin_user)
+      sign_in user
+
+      get admin_drivers_path
+      expect(response).to have_http_status(200)
+    end
+
+  end
+end
