67c53737067e6cf135b6e43a9ea5f6d13f65a6a3
john <john@entelo.com>
2016-09-20 00:17:03 -0700

    undo webpack config change in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.webpack.dev_server.enabled = false
 end
