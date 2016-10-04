d8dcd9cd1ffddf2bcfcea6ac2a96d712a9ff27cf
Adam McAmis <mcamis@gmail.com>
2016-10-03 22:17:35 -0500

    Update loading state to be more informative
    Fixes #242 however this will still need another UX pass

--- webpack/driver/containers/RideListContainer.js
+++ webpack/driver/containers/RideListContainer.js
@@ -13,17 +13,21 @@ class RideListContainer extends React.Component {
 
     render() {
         const availableRides = this.props.state.driverState.rides;
+        const isFetching = this.props.state.driverState.isFetching;
+
+        let loadingIndicator;
+        if (isFetching) {
+            loadingIndicator = (<p className="display-3"><i className="fa fa-circle-o-notch fa-spin"></i> Checking for new ride requests</p>);
+        } else {
+            //TODO: Transition state to make this not jarring on very fast connections
+            loadingIndicator = (<p className="display-3">New rides will load automatically</p>);
+        }
 
         if (this.props.state.driverState.active_ride && this.props.state.driverState.active_ride.id) {
             return <ActiveRide {...this.props} ride={this.props.state.driverState.active_ride} />
 
         } else {
             if (this.props.state.driverState.available) {
-
-                //   <div className="container">
-                  //          <div className="text-center nearby">Nearby Ride Requests</div>
-                      //        {availableRides.map((ride, i) => <PendingRide {...this.props} key={i} i={i} ride={ride} />)}
-                    //        <button className="btn btn-danger btn-bottom" onC
                 if (availableRides.length) {
                     return (
 
@@ -38,8 +42,10 @@ class RideListContainer extends React.Component {
                     return (
                         <div className="searching-container">
                             <div className="jumbotron text-center">
-                                <h1><i className="fa fa-circle-o-notch fa-spin text-info"></i></h1>
-                                <p><strong>Looking for voters...</strong></p>
+                                <h1><i className="fa fa-map-o text-info"></i></h1>
+                                <p>No voters in your area currently need a ride</p>
+                                <p className="m-t-md display-3"><strong className="text-success"><i className="fa fa-check-circle-o"></i> Connected to Dispatch</strong></p>                               
+                                {loadingIndicator}
                             </div>
                            <UnavailableButton submitUnavailable={this.props.submitUnavailable} />
                         </div>
