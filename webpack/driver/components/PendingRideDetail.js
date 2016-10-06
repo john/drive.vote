0f049c56a661128a63ef3fbd633fb630a88fd21a
Adam McAmis <mcamis@gmail.com>
2016-10-05 20:32:43 -0500

    Jump back and forth between pending ride details

--- webpack/driver/components/PendingRideDetail.js
+++ webpack/driver/components/PendingRideDetail.js
@@ -7,23 +7,26 @@ class PendingRideDetail extends React.Component {
 
 
     render() {
+        console.log('ride props', this.props);
         const ride = this.props.ride;
         const passengers = 1 + parseInt(ride.additional_passengers);
         let fromMapLink = `https://www.google.com/maps?saddr=My+Location&daddr=${ride.from_address}, ${ride.from_city}, ${ride.from_state}`;
 
         return (
-            <div className="panel">
-                <a className="btn btn-info btn-sm pull-right" target="_blank" href={fromMapLink}><i className="fa fa-map-marker"></i> Directions</a>
-                    <label>Picking up:</label>
-                    <h3>{ride.name}</h3>
-                <p>
-                    {ride.from_address}<br />
-                    {ride.from_city}, {ride.from_state} {ride.from_zip}
-                </p>
-                <div className="secondary-info">
-                    <p>Total Passengers: {passengers}</p>
-                    <p>Description: {ride.description}</p>
-                    <p>Special requests: {ride.special_requests}</p>
+            <div className="panel panel-full">
+                <div className="rideDetails">
+                    <a className="btn btn-info btn-sm pull-right" target="_blank" href={fromMapLink}><i className="fa fa-map-marker"></i> Directions</a>
+                        <label>Picking up:</label>
+                        <h3>{ride.name}</h3>
+                    <p>
+                        {ride.from_address}<br />
+                        {ride.from_city}, {ride.from_state} {ride.from_zip}
+                    </p>
+                    <div className="secondary-info">
+                        <p>Total Passengers: {passengers}</p>
+                        <p>Description: {ride.description}</p>
+                        <p>Special requests: {ride.special_requests}</p>
+                    </div>
                 </div>
                 <div className="bottom-controls secondary">
                     <button className="btn btn-success btn-lg" onClick={()=>this.props.claimRide(ride)}>Accept Ride</button>
