858128226bb5be313801c89446a38f375c019b53
Adam McAmis <mcamis@gmail.com>
2016-10-05 20:59:06 -0500

    Add SMS and Phone, fixes #374

--- webpack/driver/components/PendingRideDetail.js
+++ webpack/driver/components/PendingRideDetail.js
@@ -11,25 +11,35 @@ class PendingRideDetail extends React.Component {
         const ride = this.props.ride;
         const passengers = 1 + parseInt(ride.additional_passengers);
         let fromMapLink = `https://www.google.com/maps?saddr=My+Location&daddr=${ride.from_address}, ${ride.from_city}, ${ride.from_state}`;
+        let tel = `tel:${ride.voter_phone_number}`;
+        let sms = `sms:${ride.voter_phone_number}`;
 
         return (
             <div className="panel panel-full">
                 <div className="rideDetails">
-                    <a className="btn btn-info btn-sm pull-right" target="_blank" href={fromMapLink}><i className="fa fa-map-marker"></i> Directions</a>
-                        <label>Picking up:</label>
+                    <a className="directionsLink pull-right" target="_blank" href={fromMapLink}><i className="fa fa-map-marker"></i> Directions</a>
+                        <label>Accept Ride:</label>
                         <h3>{ride.name}</h3>
                     <p>
                         {ride.from_address}<br />
                         {ride.from_city}, {ride.from_state} {ride.from_zip}
                     </p>
-                    <div className="secondary-info">
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <a className="btn btn-gray btn-block" href={tel}><i className="fa fa-phone p-r"></i>Call</a>
+                        </div>
+                        <div className="col-xs-6">
+                            <a className="btn btn-gray btn-block" href={sms}><i className="fa fa-comment p-r"></i>Message</a>
+                        </div>
+                    </div>
+                    <div className="secondary-info m-t">
                         <p>Total Passengers: {passengers}</p>
                         <p>Description: {ride.description}</p>
                         <p>Special requests: {ride.special_requests}</p>
                     </div>
                 </div>
                 <div className="bottom-controls secondary">
-                    <button className="btn btn-success btn-lg" onClick={()=>this.props.claimRide(ride)}>Accept Ride</button>
+                    <button className="btn btn-success btn-lg" onClick={()=>this.props.claimRide(ride)}>Accept</button>
                     <button className="btn btn-secondary" onClick={()=>this.props.declineRide()}>Back to list of Voters</button>
                 </div>
             </div>
