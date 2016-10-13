08875edae65bac67fd8cf0ceb65b5255505cf72f
john <john@entelo.com>
2016-10-13 16:23:59 -0700

    add email to ride zone, step 1

--- spec/factories/ride_zone_factory.rb
+++ spec/factories/ride_zone_factory.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     sequence(:slug) { |n| "slug #{n}"}
     zip '15106'
     sequence(:phone_number) { |n| "207-867-%04d" % n }
+    sequence(:email) { |n| "phu%04d@test.com" % n }
     time_zone 'America/New_York'
     nearby_radius 5
   end
