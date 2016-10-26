5beb8dc4aa857a807f821afd951252736011a1f8
Nick Messick <nmessick@gmail.com>
2016-10-26 09:55:43 -0700

    add method to check if Conversation.from_phone is blacklisted

--- app/models/blacklisted_phone.rb
+++ app/models/blacklisted_phone.rb
@@ -1,3 +1,7 @@
 class BlacklistedPhone < ApplicationRecord
   belongs_to :conversation
+
+  def self.has_voter_phone? voter_phone
+    self.where(phone: voter_phone).empty? ? false : true
+  end
 end
