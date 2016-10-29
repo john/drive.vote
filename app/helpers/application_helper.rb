3d6804e3255547fed6aa66a3ab0fa8d453f43eef
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2016-10-29 12:44:29 -0700

    Escape HTML in flash messages
    Resolves #603, #604, #605, #588, #558

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,19 @@ module ApplicationHelper
     (request.fullpath.include?('admin') || request.fullpath.include?('dispatch')) ? false : true
   end
 
+  # Modified from humane_flash_messages to escape HTML in message content.
+  def escaped_humane_flash_messages
+    content = ""
+    unless flash[:notice].blank?
+      content << javascript_tag("humane.notice(\"#{escape_javascript(html_escape flash[:notice])}\");")
+    end
+    unless flash[:error].blank?
+      content << javascript_tag("humane.error(\"#{escape_javascript(html_escape flash[:error])}\");")
+    end
+    unless flash[:alert].blank?
+      content << javascript_tag("humane.alert(\"#{escape_javascript(html_escape flash[:alert])}\");")
+    end
+    content
+  end
+
 end
