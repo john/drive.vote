e0d6705e205586d10a8d61e758c78bf0eed29752
john <john@fryolator.com>
2016-11-06 22:37:00 -0800

    fix spec

--- spec/controllers/rides_controller_spec.rb
+++ spec/controllers/rides_controller_spec.rb
@@ -28,49 +28,40 @@ RSpec.describe RidesController, type: :controller do
   end
 
   describe "GET #create" do
-    it "redirects if not logged in" do
-      post :create, params: {id: ride.to_param}
-      expect(response).to redirect_to('/404.html')
+    let(:rz) { create :ride_zone }
+
+    let(:good_data) { {"ride_zone_id"=>rz.id, "ride"=>
+        { "name"=>"Gob", "from_address"=>"330 Cabrillo St.", "from_city_state"=>"Carnegie, PA",
+          "from_city"=>"Carnegie", "from_state"=>"PA", "pickup_at(1i)"=>"2016", "pickup_at(2i)"=>"11", "pickup_at(3i)"=>"8",
+          "pickup_at(4i)"=>"05", "pickup_at(5i)"=>"00", "phone_number"=>"2083328765"}} }
+
+    let(:bad_data) { {"ride_zone_id"=>rz.id, "ride"=>
+        { "name"=>"Gob", "from_address"=>"330 Cabrillo St.", "from_city_state"=>"Carnegie, PA",
+          "from_city"=>"Carnegie", "from_state"=>"areallylonginvalidstring", "pickup_at(1i)"=>"2016", "pickup_at(2i)"=>"11", "pickup_at(3i)"=>"8",
+          "pickup_at(4i)"=>"05", "pickup_at(5i)"=>"00", "phone_number"=>"2083328765"}} }
+
+    it "creates a user given good good data" do
+      expect {
+        post :create, params: good_data
+      }.to change{ User.count }.by(1)
     end
 
-    context "as admin" do
-      login_admin
-      let(:rz) { create :ride_zone }
+    it "creates a ride given good data" do
+      expect {
+        post :create, params: good_data
+      }.to change{ Ride.count }.by(1)
+    end
 
-      let(:good_data) { {"ride_zone_id"=>rz.id, "ride"=>
-          { "name"=>"Gob", "from_address"=>"330 Cabrillo St.", "from_city_state"=>"Carnegie, PA",
-            "from_city"=>"Carnegie", "from_state"=>"PA", "pickup_at(1i)"=>"2016", "pickup_at(2i)"=>"11", "pickup_at(3i)"=>"8",
-            "pickup_at(4i)"=>"05", "pickup_at(5i)"=>"00", "phone_number"=>"2083328765"}} }
+    it "does not create a user given bad data" do
+      expect {
+        post :create, params: bad_data
+      }.to change{ User.count }.by(0)
+    end
 
-      let(:bad_data) { {"ride_zone_id"=>rz.id, "ride"=>
-          { "name"=>"Gob", "from_address"=>"330 Cabrillo St.", "from_city_state"=>"Carnegie, PA",
-            "from_city"=>"Carnegie", "from_state"=>"areallylonginvalidstring", "pickup_at(1i)"=>"2016", "pickup_at(2i)"=>"11", "pickup_at(3i)"=>"8",
-            "pickup_at(4i)"=>"05", "pickup_at(5i)"=>"00", "phone_number"=>"2083328765"}} }
-
-      it "creates a user given good good data" do
-        expect {
-          post :create, params: good_data
-        }.to change{ User.count }.by(1)
-      end
-
-      it "creates a ride given good data" do
-        expect {
-          post :create, params: good_data
-        }.to change{ Ride.count }.by(1)
-      end
-
-      it "does not create a user given bad data" do
-        expect {
-          post :create, params: bad_data
-        }.to change{ User.count }.by(0)
-      end
-
-      it "does not create a ride given bad data" do
-        expect {
-          post :create, params: bad_data
-        }.to change{ User.count }.by(0)
-      end
-
+    it "does not create a ride given bad data" do
+      expect {
+        post :create, params: bad_data
+      }.to change{ User.count }.by(0)
     end
   end
 end
