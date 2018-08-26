851661958e56f284a3e787821057a1c42895158f
John McGrath <john@fryolator.com>
2018-08-26 13:43:39 -0700

    Admin app updates

--- app/controllers/admin/drivers_controller.rb
+++ app/controllers/admin/drivers_controller.rb
@@ -1,10 +1,20 @@
 class Admin::DriversController < Admin::AdminApplicationController
-
+  
+  DRIVER_SEARCH = "(lower(users.name) LIKE ?) OR (users.phone_number LIKE ?) OR (users.email LIKE ?) OR (lower(users.city) LIKE ?)".freeze
+  
   def index
+    page = params[:page] || 1
+    per_page = params[:per_page] || 25
+    # sort = params[:sort] || 'user.name'
+    
     if params[:rz_id].present?
       @drivers = RideZone.find(params[:rz_id])&.drivers&.to_a || []
     elsif params[:all]
       @drivers = User.all_drivers
+      
+    elsif params[:q].present?
+      @q = params[:q].downcase
+      @drivers = User.all_drivers.where(DRIVER_SEARCH, "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%").order("name DESC").paginate(page: page, per_page: per_page)
     elsif params[:assigned]
       @drivers = User.assigned_drivers
     else
